9aa928d3105f3d203b87df6589b6c497a2ba7ec9
richards01 <r13042001_@hotmail.com>
2023-08-30 18:20:15 -0500

    listo

---
+++ main.py
@@ -0,0 +1,5 @@
+# Pide el nombre del jugador por teclado
+jugador = input("Ingrese su nombre: ")
+
+# Imprime un mensaje de bienvenida con el nombre del jugador
+print(f"Bienvenid@ al juego de laberintos, {jugador}!")
