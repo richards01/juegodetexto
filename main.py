3403b28acb08e79f555eccaf60139428d47780cd
richards01 <r13042001_@hotmail.com>
2023-08-30 19:38:57 -0500

    Update main.py with nueva funcionalidad

--- main.py
+++ main.py
@@ -3,3 +3,19 @@ jugador = input("Ingrese su nombre: ")
 
 # Imprime un mensaje de bienvenida con el nombre del jugador
 print(f"Bienvenid@ al juego de laberintos, {jugador}!")
+
+import readchar
+
+def main():
+    print("Presiona ↑ para salir.")
+    
+    while True:
+        char = readchar.readkey()
+        print(f"Tecla presionada: {char}")
+        
+        if char == '\x1b[A':  # '\x1b' representa la tecla Escape, seguida por [A para ↑
+            print("Tecla ↑ presionada. Saliendo...")
+            break
+
+if __name__ == "__main__":
+    main()
